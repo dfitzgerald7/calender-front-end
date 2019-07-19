e7f903c932c6f77418d07fd011ea9887341688c5
dfitzgerald7 <dfitzge1@conncoll.edu>
2019-07-19 19:59:42 -0400

    connected CalenderContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Homepage from './components/Homepage'
+import CalenderContainer from './components/stateless/CalenderContainer'
 import './App.css';
+import calenderContainer from './components/stateless/CalenderContainer';
 
 function App() {
   return (
     <div className="appContainer">
       <Homepage />
+      <CalenderContainer/>
     </div>
   );
 }
---
+++ src/components/stateless/calenderContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CalenderContainer extends Component {
+
+    render(){
+        return (
+            <h2> Calender Goes Here</h2>
+        )
+    }
+}
+
+export default CalenderContainer
