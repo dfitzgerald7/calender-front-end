1aeaf123ca0d0b9d884a3345408eaf598ec423e1
dfitzgerald7 <dfitzge1@conncoll.edu>
2019-07-19 20:30:46 -0400

    changed cell css and render 30 components

--- src/components/stateless/CalendarComponent.js
+++ src/components/stateless/CalendarComponent.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 const CalendarComponent = props => {
+
+    const calendarArr = []
+
+    for (let i=1; i<=30; i++){
+        calendarArr.push( 
+            <div className='cell'>       
+                <h5>{i}</h5>
+                <li className='events'>
+                    To-do
+                </li>
+            </div>
+        )
+    }
+
     return (
         <div className='calendarComponent'>
-            <h5>Day</h5>
-            <li className='events'>
-                To-do
-            </li>
+            {calendarArr}
         </div>
     )
 }
